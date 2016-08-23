b9e0ae0b8cb0f38c16d5ba4be7573ec96e332e40
Motoki Kawakami <mothule.dev@gmail.com>
2016-08-23 16:24:14 +0900

    Create Projects

---
+++ RNMoVali/Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by mothule on 2016/08/23.
+//  Copyright © 2016年 mothule. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ RNMoVali/RNMoVali/RNMoVali.h
@@ -0,0 +1,19 @@
+//
+//  RNMoVali.h
+//  RNMoVali
+//
+//  Created by mothule on 2016/08/23.
+//  Copyright © 2016年 mothule. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RNMoVali.
+FOUNDATION_EXPORT double RNMoValiVersionNumber;
+
+//! Project version string for RNMoVali.
+FOUNDATION_EXPORT const unsigned char RNMoValiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RNMoVali/PublicHeader.h>
+
+
